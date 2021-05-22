a3c8cedf9bd716b61342ce59ba9bb3ff7a6d3b7d
uvsq22003018 <72202649+uvsq22003018@users.noreply.github.com>
2021-05-22 15:23:40 +0200

    Update Projet_robot_ricochet.py
    c'est bon les robot peuvent aller partout sauf au centre

--- Projet_robot_ricochet.py
+++ Projet_robot_ricochet.py
@@ -7,34 +7,35 @@
 #https://github.com/Danyt13/Projet_Robot_Ricochet-
 ########################################
 
-
 import tkinter as tk 
 import random
 
-""" creation de la fenetre """
+""" création de la fenètre """
 
 fen = tk.Tk()
 fen.title("Robot Ricochet")
 canwidth, canheight = 640, 640
 canvas = tk.Canvas(fen, width = canwidth, height = canheight, bg = "white")
 canvas.grid(column = 5, row = 5, rowspan = 5)
-position = [40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600]
+position = [0, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600]
 position1 = [0, 40, 80, 120, 160, 200, 240, 400, 440, 480, 520, 560, 600]
 objet = []
 
-"""initialisation des variable """
+"""initialisation des variables """
+
 robot1_startX, robot1_startY = 0, 0
 robot2_startX, robot2_startY = 0, 0
 robot3_startX, robot3_startY = 0, 0
 robot4_startX, robot4_startY = 0, 0
-cpt = 0 
+cpt = 0
 
-""" creation de la gille de jeu """
+""" création de la gille de jeu """
 def grille():
     for i in range(16):
      canvas.create_line(0, 40 * i, canheight, 40 * i, fill = "grey")
     for i in range(16):
      canvas.create_line(40 * i, 0, 40 * i, canwidth, fill = "grey")
+    
     canvas.create_rectangle(280 ,280, 360, 360, fill = "black")
     canvas.create_line(640, 0, 640, 640, fill = "black", width= 4)
     canvas.create_line(0, 0, 0, 640, fill = "black", width= 10)
@@ -42,18 +43,18 @@ def grille():
     canvas.create_line(0, 640, 640, 640, fill = "black", width= 4)
 grille()
 
-""" creation des murs exterieurs """ 
+""" création des murs extérieurs """ 
 
-l1 = [80, 120, 160, 200, 240, 280, 320]
-l2 = [360, 400, 440,480, 520, 560, 600]
+liste1 = [80, 120, 160, 200, 240, 280, 320]
+liste2 = [360, 400, 440,480, 520, 560, 600]
 
 def mursexterieur(): 
-     x = l1[random.randint(0, 6)]
-     x1 = l2[random.randint(0, 6)]
+     x = liste1[random.randint(0, 6)]
+     x1 = liste2[random.randint(0, 6)]
      x3 = 0
      x4 = 640
-     y2 = l1[random.randint(0, 6)]
-     y3 = l2[random.randint(0, 6)]
+     y2 = liste1[random.randint(0, 6)]
+     y3 = liste2[random.randint(0, 6)]
      y = 0
      y1 = 640
      canvas.create_line(x, y, x, y + 40, fill = "black", width= 5)
@@ -66,68 +67,125 @@ def mursexterieur():
      canvas.create_line(x4, y3, x4 - 40, y3, fill = "black", width= 5)
 mursexterieur()
 
-""" creation des murs """
+""" création des murs intérieurs """
 def mur1():
-    x = position[random.randint(0, 14)]
-    y = position[random.randint(0, 14)]
+    x = position[random.randint(1, 15)]
+    y = position[random.randint(1, 15)]
     canvas.create_line(x, y, x, y + 40, fill = "black", width= 5)
     canvas.create_line(x, y, x + 40, y, fill = "black", width= 5)
 
 def mur2():
-    x = position[random.randint(0, 14)]
-    y = position[random.randint(0, 14)]
+    x = position[random.randint(1, 15)]
+    y = position[random.randint(1, 15)]
     canvas.create_line(x, y, x, y - 40, fill = "black", width= 5)
     canvas.create_line(x, y, x + 40, y, fill = "black", width= 5)
 
 def mur3():
-    x = position[random.randint(0, 14)]
-    y = position[random.randint(0, 14)]
+    x = position[random.randint(1, 15)]
+    y = position[random.randint(1, 15)]
     canvas.create_line(x, y, x, y - 40, fill = "black", width= 5)
     canvas.create_line(x, y, x - 40, y, fill = "black", width= 5)
 
 def mur4():
-    x = position[random.randint(0, 14)]
-    y = position[random.randint(0, 14)]
+    x = position[random.randint(1, 15)]
+    y = position[random.randint(1, 15)]
     canvas.create_line(x, y, x, y + 40, fill = "black", width= 5)
     canvas.create_line(x, y, x - 40, y, fill = "black", width= 5)
 
-""" creation des robots """
+"""apparition aléatoire des murs intérieurs """
 
+for i in range(25):
+    x = random.randint(0, 3)
+    if x == 0:
+        mur1()
+    elif x == 1:
+        mur2()
+    elif x == 2:
+        mur3()
+    elif x == 3:
+        mur4()
+
+""" création des robots """
+
+#ancienne version
+"""def robotbleu():
+    global x2, y2, robot3_startX, robot3_startY
+    x2 = position[random.randint(0, 15)]
+    y2 = position[random.randint(0, 15)]
+    cercle3 = canvas.create_oval(x2, y2, x2 + 40, y2 + 40, fill = "blue")
+    robot3_startX, robot3_startY =  x2, y2
+    return cercle3 """
+
+# création de robot qui n'apparaitrons pas dans le carré du milieu
 def robotrouge():
     global x0, y0, robot1_startX, robot1_startY 
-    x0 = position1[random.randint(0, 12)]
-    y0 = position1[random.randint(0, 12)]
+    x0 = position[random.randint(0, 15)]
+    y0 = position[random.randint(0, 15)]
+    if x0 == 280 or x0 == 320:
+        y0 = position1[random.randint(0, 12)]
+    else:
+        y0 = position[random.randint(0, 15)]
+    if y0 == 280 or y0 == 320:
+        x0 = position1[random.randint(0, 12)]
+    else:
+        x0 = position[random.randint(0, 15)]
     cercle1 = canvas.create_oval(x0, y0, x0 + 40, y0 + 40, fill = "red")
     robot1_startX, robot1_startY =  x0, y0
     return cercle1
+
 def robotjaune():
     global x1, y1, robot2_startX, robot2_startY
-    x1 = position1[random.randint(0, 12)]
-    y1 = position1[random.randint(0, 12)]
+    x1 = position[random.randint(0, 15)]
+    y1 = position[random.randint(0, 15)]
+    if x1 == 280 or x1 == 320:
+        y1 = position1[random.randint(0, 12)]
+    else:
+        y1 = position[random.randint(0, 15)]
+    if y1 == 280 or y1 == 320:
+        x1 = position1[random.randint(0, 12)]
+    else:
+        x1 = position[random.randint(0, 15)]
     cercle2 = canvas.create_oval(x1, y1, x1 + 40, y1 + 40, fill = "yellow")
     robot2_startX, robot2_startY =  x1, y1
     return cercle2
+
 def robotbleu():
     global x2, y2, robot3_startX, robot3_startY
-    x2 = position1[random.randint(0, 12)]
-    y2 = position1[random.randint(0, 12)]
+    x2 = position[random.randint(0, 15)]
+    y2 = position[random.randint(0, 15)]
+    if x2 == 280 or x2 == 320:
+        y2 = position1[random.randint(0, 12)]
+    else:
+        y2 = position[random.randint(0, 15)]
+    if y2 == 280 or y2 == 320:
+        x2 = position1[random.randint(0, 12)]
+    else:
+        x2 = position[random.randint(0, 15)]
     cercle3 = canvas.create_oval(x2, y2, x2 + 40, y2 + 40, fill = "blue")
     robot3_startX, robot3_startY =  x2, y2
     return cercle3
+
 def robotvert():
     global x3, y3, robot4_startX, robot4_startY
-    x3 = position1[random.randint(0, 12)]
-    y3 = position1[random.randint(0, 12)]
+    x3 = position[random.randint(0, 15)]
+    y3 = position[random.randint(0, 15)]
+    if x3 == 280 or x3 == 320:
+        y3 = position1[random.randint(0, 12)]
+    else:
+        y3 = position[random.randint(0, 15)]
+    if y3 == 280 or y3 == 320:
+        x3 = position1[random.randint(0, 12)]
+    else:
+        x3 = position[random.randint(0, 15)]
     cercle4 = canvas.create_oval(x3, y3, x3 + 40, y3 + 40, fill = "green")
     robot4_startX, robot4_startY =  x3, y3
     return cercle4
-     
-""" creation des cibles """
 
 robot1, robot2, robot3, robot4 = robotrouge(), robotjaune(),robotbleu(), robotvert()
 
 listeDeRobot = [robot1, robot2, robot3, robot4]
 
+""" création des cibles """
 
 def ciblerouge():
     if len(objet) != 0:
@@ -177,20 +235,8 @@ def ciblevert():
         objet.append(canvas.create_rectangle(xd, yd, xd + 40, yd + 40, fill = "green"))
         return objet
 
-"""apparition aleatoire de mur """
-for i in range(25):
-    x = random.randint(0, 3)
-    if x == 0:
-        mur1()
-    elif x == 1:
-        mur2()
-    elif x == 2:
-        mur3()
-    elif x == 3:
-        mur4()
 
-
-""" deplacement des robot pour l'intant une case par une case mais je ne vois pas ou est l'erreur"""
+""" déplacement des robot, je ne vois pas ou est l'erreur"""
 
 def deplacementRobot(event):
     global x0, y0, x1, y1, x2, y2, x3, y3, cpt
@@ -277,7 +323,7 @@ canvas.bind("<Down>", deplacementRobot)
 canvas.bind("<Right>", deplacementRobot)
 canvas.bind("<Left>", deplacementRobot)
 
-""" la partie revien au debut quand on clique sur le bouton du milieu """
+""" la partie revien au début quand on clique sur le bouton du milieu """
 def recommencer():
     print("reset")
     global robot1_startX, robot1_startY, robot2_startX, robot2_startY, robot3_startX, robot3_startY, robot4_startX, robot4_startY
@@ -305,7 +351,7 @@ boutonbleu = tk.Button(fen, bg = "blue",width = 8, height = 2, command= cibleble
 boutonbleu.grid(column = 1, row = 5, rowspan = 5)
 
 
-""" widget pour score et nombre de coup """
+""" widgets pour score et nombres de coups """
 
 nombredecoup = tk.Button(fen,text = "Nombre de coup: " + str(cpt))
 nombredecoup.grid(column = 1, row = 0, rowspan = 1)
@@ -318,10 +364,10 @@ def miseajourscore(event):
     global var_score 
     pass
 
-""" configuration du curseur et de la fenetre"""
+""" configuration du curseur et de la fenètre"""
 
 canvas.configure(cursor ='hand2')
 fen.resizable(width=False, height=False)
 
-#apparition de la fenetre Tkinter
+#apparition de la fenètre Tkinter
 fen.mainloop()
